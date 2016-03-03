a222c030baebd1adc6137fc819f587c44e6f8fba
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-03 12:42:18 -0800

    connected account and book models

--- models/account.js
+++ models/account.js
@@ -2,14 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
+var Book = require('./book');
+
 var Account = new Schema({
     username: String,
-    password: String
+    password: String,
+    books: [Book.schema]
 });
 
 Account.plugin(passportLocalMongoose);
 
-
 var Account = mongoose.model('Account', Account);
+
 module.exports = Account;
 
